980a4d6207b371210b298277e55b253463dcb87f
Samuel Tseng <samuel.tseng@homeday.de>
2022-10-23 19:40:36 +0200

    first commit

---
+++ data_pipeline.py
@@ -0,0 +1,18 @@
+def clean_raw_data_in_parquet_format():
+    pass
+
+
+def clean_field_voucher_amount():
+    pass
+
+
+def clean_field_total_orders():
+    pass
+
+
+def convert_string_to_timestamp():
+    pass
+
+
+def deduplication():
+    pass
